d8bd52fdf2f35abc6dfcb1b6f6962d67e1874047
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2022-04-14 23:23:36 +0300

    Update TwitterMultiActionsV2.py

--- TwitterMultiActionsV2.py
+++ TwitterMultiActionsV2.py
@@ -1,16 +1,17 @@
 from pyuseragents import random as random_useragent
-from requests import Session, get
+from requests import Session, get, post
 from random import choice, randint
 from time import sleep
 from msvcrt import getch
-from os import system
+from os import system, listdir
 from ctypes import windll
 from urllib3 import disable_warnings
 from loguru import logger
 from sys import stderr, exit
 from json import loads
 from bs4 import BeautifulSoup
-
+from base64 import b64encode
+from names import get_full_name
 
 disable_warnings()
 def clear(): return system('cls')
@@ -20,17 +21,19 @@ windll.kernel32.SetConsoleTitleW('TwitterMultiActions V2 | by NAZAVOD')
 print('Telegram channel - https://t.me/n4z4v0d\n')
 
 
-with open('accounts.txt', 'r') as file:
+with open('accounts.txt', 'r', encoding = 'utf-8') as file:
 	accounts_cookies = [row.strip() for row in file]
 
 
-user_action = int(input('1. Массовые подписки\n2. Массовый анфолоовинг\n3. Массовые ретвиты\n4. Массовые лайки\n5. Массовые комментарии кошелька под постом\n6. Подписка между аккаунтами\n7. Сделать твит с каждого аккаунта\n8. Изменить @username на каждом аккаунте\nВведите номер вашего действия: '))
+user_action = int(input('1. Массовые подписки\n2. Массовый анфолловинг\n3. Массовые ретвиты\n4. Массовые лайки\n5. Массовые комментарии\n6. Подписка между аккаунтами\n7. Сделать твит с каждого аккаунта\n8. Изменить @username на каждом аккаунте\n9. Изменить аватарку на каждом аккаунте\n10. Изменить баннер на каждом аккаунте\n11. Изменить БИО на каждом аккаунте\n12. Изменить имя на каждом аккаунте\nВведите номер вашего действия: '))
 print('')
 
 wallets_addresses = True
+all_images_files = []
 
 if user_action in (1, 2):
-	username_to_subscribe = str(input('Введите @username профиля: ')).replace('@', '')
+	username_to_subscribe = str(input('Введите @username профиля (профилей, разделять через запятую, без пробелов): ')).replace('@', '').replace(' ', '')
+	username_to_subscribe_list = username_to_subscribe.split(',')
 
 elif user_action == 5:
 	tweet_url = str(input('Введите ссылку на твит: '))
@@ -41,15 +44,18 @@ elif user_action == 5:
 	if tag_users == 'y':
 		how_much_users_tag = int(input('Сколько друзей необходимо отметить?: '))
 	
-	need_phrase_for_comment = str(input('Добавить вашу фразу к комментарию? (y/N): ')).lower
+	need_phrase_for_comment = str(input('Добавить вашу фразу к комментарию? (y/N): ')).lower()
 
 	if need_phrase_for_comment == 'y':
 		phrase_for_comment = str(input('Введите фразу: '))
 
-	wallets_txt_folder = str(input('Перетяните .txt файл с кошельками: '))
+	need_send_wallet = str(input('Отправлять кошельки из .txt в комментарии? (y/N): ')).lower()
 
-	with open(wallets_txt_folder, 'r') as file:
-		wallets_addresses = [row.strip() for row in file]
+	if need_send_wallet == 'y':
+		wallets_txt_folder = str(input('Перетяните .txt файл с кошельками: '))
+
+		with open(wallets_txt_folder, 'r', encoding = 'utf-8') as file:
+			wallets_addresses = [row.strip() for row in file]
 
 elif user_action == 7:
 	text_to_tweet_source = int(input('Выберите способ загрузка текста для отправки твита (1 - ввести в консоль; 2 - перетянуть файл): '))
@@ -61,13 +67,24 @@ elif user_action == 7:
 	else:
 		current_text_to_tweet = str(input('Введите текст для твита: '))
 
-	with open(text_to_tweet_folder, 'r') as file:
+	with open(text_to_tweet_folder, 'r', encoding = 'utf-8') as file:
 		text_to_tweet_list = [row.strip() for row in file]
-
 
 elif user_action in (3, 4):
 	tweet_url = str(input('Введите ссылку на твит: '))
 	tweet_id = tweet_url.split('status/')[-1].split('/')[0].split('?')[0].split('&')[0].replace(' ', '')
+
+elif user_action == 9:
+	avatars_source = int(input('Выберите способ загрузки аватарок (1 - из файла; 2 - случайные с thispersondoesnotexist.com): '))
+
+	if avatars_source == 1:
+		images_folder = str(input('Перетяните ПАПКУ с аватарками: '))
+
+elif user_action == 10:
+	images_folder = str(input('Перетяните ПАПКУ с баннерами: '))
+
+elif user_action == 11:
+	bio_source = str(input('Перетяните .txt файл с БИО (каждый с новой строки): '))
 
 user_sleep_option = str(input('Использовать задержку между выполнением действий? (y/N): ')).lower()
 
@@ -82,11 +99,19 @@ if use_proxies == 'y':
 
 
 def random_proxy_from_file():
-	with open(proxy_folder, 'r') as file:
+	with open(proxy_folder, 'r', encoding = 'utf-8') as file:
 		proxies = file.readlines()
 		
 	return(choice(proxies))
 
+def get_all_images_in_folder():
+	return(listdir(images_folder))
+
+def get_random_bio_from_file():
+	with open(bio_source, 'r', encoding = 'utf-8') as file:
+		bio = file.readlines()
+
+	return(choice(bio))
 
 class Wrong_Response(BaseException):
 	def __init__(self, message):
@@ -111,12 +136,11 @@ class MainWork():
 					raise Wrong_Response(r)
 
 				url_to_get_query_ids = BeautifulSoup(r.text, 'lxml').find_all('link', {'rel': 'preload', 'as': 'script', 'crossorigin': 'anonymous'})[-1].get('href')
-
 				r = self.session.get(url_to_get_query_ids, verify = False)
 				self.queryIdforSubscribe = r.text.split('",operationName:"TweetResultByRestId')[0].split('"')[-1]
 				self.queryIdforRetweet = r.text.split('",operationName:"CreateRetweet')[0].split('"')[-1]
 				self.queryIdforLike = r.text.split('",operationName:"FavoriteTweet')[0].split('"')[-1]
-				self.queryIdforComment = r.text.split('",operationName:"ListProductSubscriptions')[-1].split('"')[-1]
+				self.queryIdforComment = r.text.split('",operationName:"CreateTweet"')[0].split('"')[-1]
 				self.queryIdforFollowers = r.text.split('",operationName:"Followers')[0].split('"')[-1]
 				self.queryIdforUserByScreenName = r.text.split('",operationName:"UserByScreenName')[0].split('"')[-1]
 				self.queryIdforCreateTweet = r.text.split('",operationName:"CreateTweet"')[0].split('"')[-1]
@@ -126,26 +150,37 @@ class MainWork():
 
 			except Exception as error:
 				logger.error(f'Ошибка при получении начальных параметров: {str(error)}')
+				continue
 
 			except Wrong_Response as error:
 				self.session.headers.update({'user-agent': random_useragent()})
+				continue
 
 			else:
 				break
 
 	def get_username(self):
-		r = self.session.get('https://mobile.twitter.com/i/api/1.1/account/settings.json?include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true&include_ext_sharing_audiospaces_listening_data_with_followers=true', verify=False)
+		for _ in range(3):
+			r = self.session.get('https://mobile.twitter.com/i/api/1.1/account/settings.json?include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true&include_ext_sharing_audiospaces_listening_data_with_followers=true', verify=False)
 
-		try:
-			self.username = str(loads(r.text)['screen_name'])
+			try:
+				if not loads(r.text).get('errors'):
+					self.username = str(loads(r.text)['screen_name'])
+				
+				else:
+					raise Wrong_Response(r)
 
-		except Exception as error:
-			response = r.text.replace('\n', '').replace('\r', '')
-			logger.error(f'Ошибка при получении @username: {str(error)}, ответ: {response}, строка: {self.cookies_str}')
-			return(False, None)
+			except Exception as error:
+				response = r.text.replace('\n', '').replace('\r', '')
+				logger.error(f'Ошибка при получении @username: {str(error)}, ответ: {response}, строка: {self.cookies_str}')
 
-		else:
-			return(True, self.username)
+			except Wrong_Response as error:
+				logger.error(f'Ошибка при получении @username: {str(error)}, код ответа: {str(r.status_code)}, ответ: {str(r.text)}, строка: {self.cookies_str}')
+
+			else:
+				return(True, self.username)
+
+		return(False, None)
 
 	def mass_follow(self, current_username_to_subscribe):
 		if self.username != current_username_to_subscribe:
@@ -215,26 +250,25 @@ class MainWork():
 	def mass_comments(self, address):
 		for _ in range(3):
 			try:
+				fullmesage = ''
+
 				if tag_users == 'y':
 					users_to_tag = []
 
 					for _ in range(how_much_users_tag):
 						first3 = "".join([choice("abcdefghijklmnopqrstuvwxyz013456789") for _ in range(3)])
-						r = self.session.get('https://twitter.com/i/api/1.1/search/typeahead.json?q=' + str(first3)+'&src=compose&result_type=users&context_text=' + str(first3))
+						r = self.session.get('https://twitter.com/i/api/1.1/search/typeahead.json?q=' + str(first3) + '&src=compose&result_type=users&context_text=' + str(first3))
 						users_to_tag.append('@' + loads(r.text)['users'][0]['screen_name'])
 
-					if need_phrase_for_comment == 'y':
-						fullmesage = ' \\n'.join(users_to_tag) + '\\n' + address + '\\n' + phrase_for_comment
-					else:
-						fullmesage = ' \\n'.join(users_to_tag) + '\\n' + address
-				
-				else:
-					if need_phrase_for_comment == 'y':
-						fullmesage = '\\n' + address + '\\n' + phrase_for_comment
-					else:
-						fullmesage = '\\n' + address
+					fullmesage += '\\n'.join(users_to_tag)
 
-				r = self.session.post(f'https://mobile.twitter.com/i/api/graphql/{self.queryIdforComment}/CreateTweet', headers={'content-type': 'application/json'}, json = {"variables":"{\"tweet_text\":\"" + fullmesage + "\",\"reply\":{\"in_reply_to_tweet_id\":\"" + tweet_id + "\",\"exclude_reply_user_ids\":[]},\"media\":{\"media_entities\":[],\"possibly_sensitive\":false},\"withDownvotePerspective\":false,\"withReactionsMetadata\":false,\"withReactionsPerspective\":false,\"withSuperFollowsTweetFields\":true,\"withSuperFollowsUserFields\":true,\"semantic_annotation_ids\":[],\"dark_request\":false,\"withUserResults\":true,\"withBirdwatchPivots\":false}","queryId":"" + self.queryIdforComment + ""}, verify = False)
+				if need_phrase_for_comment == 'y':
+					fullmesage += '\\n' + phrase_for_comment
+
+				if address:
+					fullmesage += '\\n' + address
+
+				r = self.session.post(f'https://twitter.com/i/api/graphql/{self.queryIdforComment}/CreateTweet', headers = {'content-type': 'application/json'}, json = {"variables":"{\"tweet_text\":\"" + fullmesage + "\",\"reply\":{\"in_reply_to_tweet_id\":\"" + tweet_id + "\",\"exclude_reply_user_ids\":[]},\"media\":{\"media_entities\":[],\"possibly_sensitive\":false},\"withDownvotePerspective\":false,\"withReactionsMetadata\":false,\"withReactionsPerspective\":false,\"withSuperFollowsTweetFields\":true,\"withSuperFollowsUserFields\":true,\"semantic_annotation_ids\":[],\"dark_request\":false,\"withUserResults\":true,\"withBirdwatchPivots\":false}","queryId":"" + self.queryIdforComment + ""}, verify = False)
 
 				if not r.ok:
 					raise Wrong_Response(r)
@@ -269,10 +303,10 @@ class MainWork():
 
 				return
 
-	def mass_unfollow(self):
+	def mass_unfollow(self, current_username_to_subscribe):
 		for _ in range(3):
 			try:
-				r = self.session.get(f'https://mobile.twitter.com/i/api/graphql/{self.queryIdforUserByScreenName}' + '/UserByScreenName?variables={"screen_name":"'+username_to_subscribe + '","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}', headers = {'content-type': 'application/x-www-form-urlencoded'}, verify = False)
+				r = self.session.get(f'https://mobile.twitter.com/i/api/graphql/{self.queryIdforUserByScreenName}' + '/UserByScreenName?variables={"screen_name":"'+ current_username_to_subscribe + '","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}', headers = {'content-type': 'application/x-www-form-urlencoded'}, verify = False)
 
 				if not r.ok:
 					raise Wrong_Response(r)
@@ -291,7 +325,7 @@ class MainWork():
 				logger.error(f'{self.username} | Ошибка при массовом анфолловинге: {str(error)}, код ответа: {str(r.status_code)}, ответ: {str(r.text)}')
 
 			else:
-				logger.success(f'{self.username} | Аккаунт успешно отписался от {username_to_subscribe}')
+				logger.success(f'{self.username} | Аккаунт успешно отписался от {current_username_to_subscribe}')
 
 				return
 
@@ -335,6 +369,90 @@ class MainWork():
 				logger.success(f'{self.username} | Аккаунт сменил @username на {random_username}')
 
 				return
+
+	def get_random_avatar_from_web(self):
+		r = get('https://thispersondoesnotexist.com/image')
+		return(b64encode(r.content))
+
+	def get_random_image_from_list(self, folder):
+		global all_images_files
+
+		if len(all_images_files) < 1:
+			all_images_files = get_all_images_in_folder()
+
+		random_file = choice(all_images_files)
+		all_images_files.remove(random_file)
+
+		return(b64encode(open(f'{folder}\\{random_file}', 'rb').read()))
+
+	def change_avatar(self, avatar_data):
+		for _ in range(3):
+			try:
+				r = self.session.post("https://twitter.com/i/api/1.1/account/update_profile_image.json", data = {"image": avatar_data}, headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+				if r.status_code != 200:
+					raise Wrong_Response(r)
+
+			except Exception as error:
+				logger.error(f'{self.username} | Ошибка при смене аватарки: {str(error)}')
+
+			except Wrong_Response as error:
+				logger.error(f'{self.username} | Ошибка при смене аватарки: {str(error)}, код ответа: {str(r.status_code)}, ответ: {str(r.text)}')
+
+			else:
+				logger.success(f'{self.username} | Аккаунт успешно сменил аватарку')
+
+				return
+
+	def change_banner(self, banner_data):
+		for _ in range(3):
+			try:
+				r = self.session.post("https://twitter.com/i/api/1.1/account/update_profile_banner.json", data = {"banner": banner_data}, headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+				if r.status_code != 201:
+					raise Wrong_Response(r)
+
+			except Exception as error:
+				logger.error(f'{self.username} | Ошибка при смене баннера: {str(error)}')
+
+			except Wrong_Response as error:
+				logger.error(f'{self.username} | Ошибка при смене баннера: {str(error)}, код ответа: {str(r.status_code)}, ответ: {str(r.text)}')
+
+			else:
+				logger.success(f'{self.username} | Аккаунт успешно сменил баннер')
+
+				return
+
+	def change_profile(self, current_bio):
+		for _ in range(3):
+			try:
+				r = self.session.get('https://twitter.com/i/api/graphql/Bhlf1dYJ3bYCKmLfeEQ31A/UserByScreenName?variables={"screen_name":"'+ self.username +'","withSafetyModeUserFields":true,"withSuperFollowsUserFields":true}')
+
+				if not r.ok:
+					raise Wrong_Response(r)
+
+				old_desc = loads(r.text)['data']['user']['result']['legacy']['description']
+				old_name = loads(r.text)['data']['user']['result']['legacy']['name']
+
+				if not current_bio:
+					r = self.session.post('https://twitter.com/i/api/1.1/account/update_profile.json', headers = {'content-type': 'application/x-www-form-urlencoded'}, data = f'displayNameMaxLength=50&name={get_full_name()}&description={old_desc}&location='.encode('utf-8'))
+
+				else:
+					r = self.session.post('https://twitter.com/i/api/1.1/account/update_profile.json', headers = {'content-type': 'application/x-www-form-urlencoded'}, data = f'displayNameMaxLength=50&name={old_name}&description={current_bio}&location='.encode('utf-8'))
+
+				if r.status_code != 200:
+					raise Wrong_Response(r)
+
+			except Exception as error:
+				logger.error(f'{self.username} | Ошибка при обновлении профиля: {str(error)}')
+
+			except Wrong_Response as error:
+				logger.error(f'{self.username} | Ошибка при обновлении профиля: {str(error)}, код ответа: {str(r.status_code)}, ответ: {str(r.text)}')
+
+			else:
+				logger.success(f'{self.username} | Аккаунт успешно сменил описание/имя')
+
+				return
 			
 
 if __name__ == '__main__':
@@ -351,53 +469,70 @@ if __name__ == '__main__':
 	for current_cookies_str in accounts_cookies:
 		if wallets_addresses or len(wallets_addresses) > 0:
 			csrf_token = current_cookies_str.split('ct0=')[-1].split(';')[0]
+			main_worker_obj = MainWork(current_cookies_str, csrf_token)
+			get_username_status, current_username = main_worker_obj.get_username()
 
-			for _ in range(15):
-				try:
-					main_worker_obj = MainWork(current_cookies_str, csrf_token)
+			if get_username_status:
+				if user_action == 1:
+					for username_to_subscribe in username_to_subscribe_list:
+						main_worker_obj.mass_follow(username_to_subscribe)
 
-					get_username_status, current_username = main_worker_obj.get_username()
+				elif user_action == 2:
+					for username_to_subscribe in username_to_subscribe_list:
+						main_worker_obj.mass_unfollow(username_to_subscribe)
 
-					if get_username_status:
-						if user_action == 1:
+				elif user_action == 3:
+					main_worker_obj.mass_retweets()
+
+				elif user_action == 4:
+					main_worker_obj.mass_likes()
+
+				elif user_action == 5:
+					if need_send_wallet == 'y':
+						main_worker_obj.mass_comments(wallets_addresses.pop(0))
+
+					else:
+						main_worker_obj.mass_comments(None)
+
+				elif user_action == 6:
+					if len(all_usernames) > 1:
+						for username_to_subscribe in all_usernames:
 							main_worker_obj.mass_follow(username_to_subscribe)
 
-						elif user_action == 2:
-							main_worker_obj.mass_unfollow()
+				elif user_action == 7:
+					if text_to_tweet_source == 2:
+						if text_to_tweet_type == 1:
+							current_text_to_tweet = text_to_tweet_list.pop(0)
+						elif text_to_tweet_type == 2:
+							current_text_to_tweet = text_to_tweet_list.pop(randint(0, len(text_to_tweet_list)-1))
+						else:
+							current_text_to_tweet = choice(text_to_tweet_list)
 
-						elif user_action == 3:
-							main_worker_obj.mass_retweets()
+					main_worker_obj.mass_tweets(current_text_to_tweet)
 
-						elif user_action == 4:
-							main_worker_obj.mass_likes()
+				elif user_action == 8:
+					main_worker_obj.change_username()
 
-						elif user_action == 5:
-							main_worker_obj.mass_comments(wallets_addresses.pop(0))
+				elif user_action == 9:
+					if avatars_source == 2:
+						avatar_data = main_worker_obj.get_random_avatar_from_web()
 
-						elif user_action == 6:
-							if len(all_usernames) > 1:
-								for username_to_subscribe in all_usernames:
-									main_worker_obj.mass_follow(username_to_subscribe)
+					else:
+						avatar_data = main_worker_obj.get_random_image_from_list(images_folder)
 
-						elif user_action == 7:
-							if text_to_tweet_source == 2:
-								if text_to_tweet_type == 1:
-									current_text_to_tweet = text_to_tweet_list.pop(0)
-								elif text_to_tweet_type == 2:
-									current_text_to_tweet = text_to_tweet_list.pop(randint(0, len(text_to_tweet_list)-1))
-								else:
-									current_text_to_tweet = choice(text_to_tweet_list)
+					main_worker_obj.change_avatar(avatar_data)
 
-							main_worker_obj.mass_tweets(current_text_to_tweet)
+				elif user_action == 10:
+					banner_data = main_worker_obj.get_random_image_from_list(images_folder)
 
-						elif user_action == 8:
-							main_worker_obj.change_username()
-				
-				except:
-					pass
+					main_worker_obj.change_banner(banner_data)
 
-				else:
-					break
+				elif user_action == 11:
+					random_bio_str = get_random_bio_from_file()
+					main_worker_obj.change_profile(random_bio_str)
+
+				elif user_action == 12:
+					main_worker_obj.change_profile(None)
 
 		else:
 			break
